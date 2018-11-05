bcb7026d705b1faa8a42a18303786bfa91f1e1b3
FredJiang <jiang.fei@ffcg.se>
2018-11-05 16:45:16 +0800

    sum

--- authorquiz/src/AuthorQuiz.test.js
+++ authorquiz/src/AuthorQuiz.test.js
@@ -5,5 +5,5 @@ import AuthorQuiz from './AuthorQuiz';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<AuthorQuiz />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.unmountComponentAtNode(div);  
 });
--- authorquiz/src/index.js
+++ authorquiz/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
+import Sum from './Sum';
 import * as serviceWorker from './serviceWorker';
 import './bootstrap.min.css'
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+ReactDOM.render(<AuthorQuiz a={'c'} b={2} />, document.getElementById('root'));
+ReactDOM.render(<Sum a ={'a'} b={2} />,  document.getElementById('root'));
 serviceWorker.unregister();
