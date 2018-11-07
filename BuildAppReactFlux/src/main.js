e90672bf85ce1eb7d39e840edc24116f3ca1533f
FredJiang <jiang.fei@ffcg.se>
2018-11-07 11:25:59 +0800

    router

--- BuildAppReactFlux/src/main.js
+++ BuildAppReactFlux/src/main.js
@@ -1,4 +1,28 @@
 var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
+var Header = require('./components/common/header');
 var React = require('react');
 
-React.render(<Home />,  document.getElementById('app'))
+var App = React.createClass({
+    render: function () {
+        var Child;
+        switch (this.props.route) {
+            case 'about': Child = About; break;
+            default: Child = Home;
+        }
+        return (
+            <div>
+                <Header />
+                <Child />
+            </div>
+        );
+    }
+});
+
+function render() {
+    var route = window.location.hash.substr(1);
+    React.render(<App route={route} />, document.getElementById('app'));
+}
+
+window.addEventListener('hashchange', render);
+render();
