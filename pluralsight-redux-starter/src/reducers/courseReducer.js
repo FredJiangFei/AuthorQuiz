5a3ba1d0907d620488035aabd736f3061380a30b
FredJiang <jiang.fei@ffcg.se>
2018-11-12 09:42:11 +0800

    kkk

--- pluralsight-redux-starter/src/reducers/courseReducer.js
+++ pluralsight-redux-starter/src/reducers/courseReducer.js
@@ -1,9 +1,10 @@
 import initialState from './initialState';
+import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
 
-    case 'CREATE_COURSE':
+    case types.CREATE_COURSE:
       return [
         ...state,
         Object.assign({}, action.course)
